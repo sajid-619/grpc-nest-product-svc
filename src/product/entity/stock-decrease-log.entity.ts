9335d246fed2b0d1eae008a47584fe6097b86a57
sajid-619 <zsajid014@gmail.com>
2022-12-18 21:33:08 +0600

    Final Commit

---
+++ src/product/entity/stock-decrease-log.entity.ts
@@ -0,0 +1,29 @@
+/* eslint-disable prettier/prettier */
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+} from 'typeorm';
+import { Product } from './product.entity';
+
+@Entity()
+export class StockDecreaseLog extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  public id!: number;
+
+  /*
+   * Relation IDs
+   */
+
+  @Column({ type: 'integer' })
+  public orderId!: number;
+
+  /*
+   * Many-To-One Relationships
+   */
+
+  @ManyToOne(() => Product, (product) => product.stockDecreaseLogs)
+  public product: Product;
+}
